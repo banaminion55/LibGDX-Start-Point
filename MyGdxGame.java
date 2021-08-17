96ce39411abcf6768f73888cb7333705e2798789
banaminion55 <75647907+banaminion55@users.noreply.github.com>
2021-08-17 14:37:09 -0700

    Add files via upload

---
+++ MyGdxGame.java
@@ -0,0 +1,17 @@
+package com.golder;
+//LibGDX = graphics library
+
+import com.badlogic.gdx.Game;
+
+/*
+- This class controls which game screen is being shown (pause screen, gameplay screen, game over screen, inventory screen, etc.)
+ */
+
+public class MyGdxGame extends Game {
+
+	@Override
+	public void create() {
+		setScreen(new GameplayScreen());
+	}
+
+}
